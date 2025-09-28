5000b28c87c0007d10ecb7cb761b10b46588861d
Bruno Saint Clair <brunosaint@dcomp.ufs.br>
2025-09-28 12:17:08 -0300

    :tada: Commit inicial

---
+++ src/test/java/api/controla_preju/ControlaPrejuApplicationTests.java
@@ -0,0 +1,13 @@
+package api.controla_preju;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlaPrejuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
