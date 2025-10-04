8f3755567ebc02e255557738e45ce45d32b4354f
Bruno Saint Clair <brunosaint@dcomp.ufs.br>
2025-10-04 11:36:54 -0300

    :test_tube: test(users): Adiciona testes unitários

---
+++ src/test/java/api/controla_preju/services/UserServiceTest.java
@@ -0,0 +1,80 @@
+package api.controla_preju.services;
+
+import api.controla_preju.dtos.forms.CreateUserForm;
+import api.controla_preju.entities.User;
+import api.controla_preju.exceptions.BusinessException;
+import api.controla_preju.repositories.UserRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class UserServiceTest {
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private PasswordEncoder passwordEncoder;
+
+    @InjectMocks
+    private UserService userService;
+
+    @Test
+    @DisplayName("Should create user succesfully")
+    void shouldCreateUser() {
+        var form = new CreateUserForm("test@email.com", "Test user", "123456");
+
+        when(userRepository.existsByEmail(form.email())).thenReturn(false);
+        when(passwordEncoder.encode(form.password())).thenReturn("encryptedpassword");
+        when(userRepository.save(any(User.class))).thenAnswer(invocation -> invocation.getArgument(0));
+
+        User createdUser = userService.create(form);
+
+        verify(userRepository).existsByEmail(form.email());
+        verify(passwordEncoder).encode(form.password());
+        verify(userRepository).save(any(User.class));
+
+        assertNotNull(createdUser);
+        assertEquals("test@email.com", createdUser.getEmail());
+        assertEquals("encryptedpassword", createdUser.getPassword());
+    }
+
+    @Test
+    @DisplayName("Should throw exception of not unique email while creating user")
+    void shouldThrowExceptionCreatingUser() {
+        var form = new CreateUserForm("email@email.com", "Test User", "123456");
+        when(userRepository.existsByEmail(form.email())).thenReturn(true);
+
+        assertThrows(BusinessException.class, () -> userService.create(form));
+        verify(userRepository, never()).save(any(User.class));
+    }
+
+    @Test
+    @DisplayName("Should deactivate user succesfully")
+    void shouldDeactivate() {
+        var user = new User("test@email.com", "test name", "password");
+        userService.deactivate(user);
+        verify(userRepository).save(user);
+        assertEquals('I', user.getStatus());
+    }
+
+    @Test
+    @DisplayName("Should reactivate user succesfully")
+    void shouldReactivate() {
+        var user = new User("test@email.com", "test name", "password");
+        assertEquals('A', user.getStatus());
+        user.setStatus('I');
+        userService.reactivate(user);
+        verify(userRepository).save(user);
+        assertEquals('A', user.getStatus());
+    }
+
+}
